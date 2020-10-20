0e6a381ad108fad765b0a8f99a3c80c9f44ff93a
pvd1302 <pvduc.ns@gmail.com>
2020-10-20 20:05:15 +0700

    ok

---
+++ bai_4.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace B2_bai4
+{
+    class DaThuc
+    {
+        private int n;
+        private int[] a;
+        public DaThuc()
+        {
+            n = 0;
+            a = null;
+        }
+        public DaThuc(int n)
+        {
+            this.n = n;
+            a = new int[n];
+        }
+        public void Nhap()
+        {
+            Console.WriteLine("Nhap thong tin cua da thuc");
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("a[{0}] = ", i);
+                a[i] = int.Parse(Console.ReadLine());
+            }
+        }
+        public void Hien()
+        {
+            Console.WriteLine("Thong tin cac he so cua da thuc");
+            for (int i = 0; i < n; i++)
+                Console.Write("{0}, ", i);
+        }
+        public DaThuc Tong(DaThuc t2)
+        {
+            if (this.n == t2.n)
+            {
+                DaThuc d = new DaThuc(this.n);
+                for (int i = 0; i < n; i++)
+                    d.a[i] = this.a[i] + t2.a[i];
+                return d;
+            }
+            else return null;
+        }
+        public DaThuc Hieu(DaThuc t2)
+        {
+            if (this.n == t2.n)
+            {
+                DaThuc d = new DaThuc(this.n);
+                for (int i = 0; i < n; i++)
+                    d.a[i] = this.a[i] - t2.a[i];
+                return d;
+            }
+            else return null;
+        }
+        class App
+        {
+            static void Main4()
+            {
+                DaThuc d1 = new DaThuc();
+                DaThuc d2 = new DaThuc();
+                Console.WriteLine("Da thuc thu nhat"); d1.Hien();
+                Console.WriteLine("Da thuc thu hai"); d2.Hien();
+                DaThuc d3 = d1.Tong(d2);
+                DaThuc d4 = d1.Hieu(d2);
+                Console.WriteLine("Da thuc tong"); d3.Hien();
+                Console.WriteLine("Da thuc hieu"); d4.Hien();
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ bai_5.cs
@@ -0,0 +1,50 @@
+﻿public class Date
+{
+    public void DisplayCurrentTime()
+    {
+        Console.WriteLine("Time\t: {0}/{1}/{2}   {3}:{4}:{5}", date, month, year, hour, minute, second);
+
+    }
+    public Date(System.DateTime dt)
+    {
+        year = dt.Year;
+        month = dt.Month;
+        date = dt.Day;
+        hour = dt.Hour;
+        minute = dt.Minute;
+        second = dt.Second;
+    }
+    public int Hour
+    {
+        get
+        {
+            return Hour;
+        }
+        set
+        {
+            hour = value;
+        }
+    }
+
+
+    private int year;
+    private int month;
+    private int date;
+    private int hour;
+    private int minute;
+    private int second;
+
+
+    static void Main5(string[] args)
+    {
+        DateTime currentTime = System.DateTime.Now;
+        Date d = new Date(currentTime);
+        d.DisplayCurrentTime();
+
+        int theHour = d.Hour;
+        Console.WriteLine("Retrieved the hour: {0} ", theHour);
+        theHour++;
+        d.Hour = theHour;
+        Console.WriteLine("Updated the hour: {0} ", theHour);
+    }
+}
